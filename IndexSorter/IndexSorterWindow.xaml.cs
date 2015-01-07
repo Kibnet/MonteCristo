f53af6da2d4e4572bf779e8ed50e920807d350f8
Kibnet <Kibnet@hotmail.com>
2015-01-08 02:24:00 +1000

    +Оптимизировал алгоритм сортировки -Удалил проект создателя индексов, так как теперь не нужен

--- IndexSorter/IndexSorterWindow.xaml.cs
+++ IndexSorter/IndexSorterWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Security;
 using System.Windows;
 
 namespace IndexSorter
@@ -16,8 +17,23 @@ namespace IndexSorter
 			InitializeComponent();
 		}
 
+		[SecuritySafeCritical]
+		public static unsafe void GetBytes(int value, ref byte[] numArray, int position = 0)
+		{
+			fixed (byte* numPtr = numArray)
+				*(int*)(numPtr+position) = value;
+		}
+
+		public static unsafe void GetBytes(long value, ref byte[] numArray)
+		{
+			fixed (byte* numPtr = numArray)
+				*(long*)numPtr = value;
+		}
+		
 		static DirectoryInfo dirin;
 		static DirectoryInfo dirout;
+		static short icnt;
+		static long maxfilesize;
 
 		private void Sort_Click(object sender, RoutedEventArgs e)
 		{
@@ -38,105 +54,136 @@ namespace IndexSorter
 			};
 			wor.DoWork += WorOnDoWork;
 
+			icnt = (short)FileName.Value.GetValueOrDefault();
+			maxfilesize = MaxFileSize.Value.GetValueOrDefault();
 			wor.RunWorkerAsync();
 		}
 
 		private void WorOnDoWork(object sender, DoWorkEventArgs doWorkEventArgs)
 		{
 			long pcnt = 0;
-			var buf = new byte[4];
-			var sort = new SortedSet<long>();
+			long dcnt = 0;
+			short len;
+			int last;
+			int lastpos;
+			var buf = new byte[12];
+			var list = new List<long>((int)maxfilesize / 4);
+			var datas = new MemoryStream((int)maxfilesize);
+			var index = new MemoryStream((int)maxfilesize);
 			var start = DateTime.Now;
-			long lon;
-			foreach (FileInfo file in dirin.EnumerateFiles("index-*"))
+
+			foreach (FileInfo file in dirin.EnumerateFiles())
 			{
-				try
+				using (var stream = file.OpenRead())
 				{
-					using (var stream = file.OpenRead())
+					try
 					{
 						while (true)
 						{
-							lon = stream.Position;
-							if (4 != stream.Read(buf, 0, 4))
+							if (8 != stream.Read(buf, 0, 8))
 							{
 								break;
 							}
-							lon += (long)(BitConverter.ToInt32(buf, 0)) << 32;
-							sort.Add(lon);
+							Array.Copy(buf, 0, buf, 8, 4);
+							list.Add(BitConverter.ToInt64(buf, 0));
+							list.Add(BitConverter.ToInt64(buf, 4));
+							dcnt += 2;
 							++pcnt;
 							if (pcnt % 100000 == 0)
 							{
-								((BackgroundWorker)sender).ReportProgress(0, file.Name + " обработано " + pcnt + " пар");
+								((BackgroundWorker)sender).ReportProgress(0, file.Name + " загружено " + pcnt.ToString("N0") + " пар");
 							}
-						}
-					}
 
-					var datfile = new FileInfo(file.FullName.Replace("index-", "datas-"));
-
-					var datbuf = File.ReadAllBytes(datfile.FullName);
-
-					using (var datas = new MemoryStream())
-					{
-						using (var index = new MemoryStream())
-						{
-							short len = 0;
-							int last = int.MinValue;
-							int lastpos = 0;
-							foreach (long l in sort)
+							
+							if (dcnt * 4 >= maxfilesize)
 							{
-								buf = BitConverter.GetBytes(l);
-								var pos = BitConverter.ToInt32(buf, 0);
-								var key = BitConverter.ToInt32(buf, 4);
-								if (last != key)
+								((BackgroundWorker)sender).ReportProgress(0, file.Name + " сортировка " + dcnt.ToString("N0") + " пар");
+								list.Sort();
+								len = 0;
+								last = int.MinValue;
+								lastpos = 0;
+								dcnt = 0;
+								foreach (long l in list)
 								{
-									if (len != 0)
+									GetBytes(l, ref buf);
+									var dat = BitConverter.ToInt32(buf, 0);
+									var key = BitConverter.ToInt32(buf, 4);
+									if (last != key)
 									{
-										index.Write(BitConverter.GetBytes(last), 0, 4);
-										index.Write(BitConverter.GetBytes(lastpos), 0, 4);
-										index.Write(BitConverter.GetBytes(len), 0, 2);
+										if (len != 0)
+										{
+											GetBytes(last, ref buf);
+											GetBytes(lastpos, ref buf, 4);
+											GetBytes(len, ref buf, 8);
+											index.Write(buf, 0, 10);
+										}
+										last = key;
+										lastpos = (int)datas.Position;
+										len = 1;
 									}
-									last = key;
-									lastpos = (int)datas.Position;
-									len = 1;
+									else
+									{
+										len++;
+									}
+									GetBytes(dat, ref buf);
+									datas.Write(buf, 0, 4);
+									++dcnt;
+									if (dcnt % 100000 == 0)
+									{
+										((BackgroundWorker)sender).ReportProgress(0, file.Name + " сохранено " + dcnt.ToString("N0") + " пар");
+									}
 								}
-								else
-								{
-									len++;
-								}
-								datas.Write(datbuf, pos, 4);
-								++pcnt;
-								if (pcnt % 100000 == 0)
-								{
-									((BackgroundWorker)sender).ReportProgress(0, file.Name + " обработано " + pcnt + " пар");
-								}
-							}
 
-							if (len != 0)
-							{
-								index.Write(BitConverter.GetBytes(last), 0, 4);
-								index.Write(BitConverter.GetBytes(lastpos), 0, 4);
-								index.Write(BitConverter.GetBytes(len), 0, 2);
-							}
-
-							using (var str = File.Create(dirout.FullName + "\\" + file.Name))
-							{
+								if (len != 0)
+								{
+									GetBytes(last, ref buf);
+									GetBytes(lastpos, ref buf, 4);
+									GetBytes(len, ref buf, 8);
+									index.Write(buf, 0, 10);
+								}
+								
+								Flush(index, datas);
 								index.Position = 0;
-								index.CopyTo(str);
+								index.SetLength(0);
+								datas.Position = 0;
+								datas.SetLength(0);
+								icnt++;
+								dcnt = 0;
+								list.Clear();
 							}
-						}
-						using (var datstr = File.Create(dirout.FullName + "\\" + datfile.Name))
-						{
-							datas.Position = 0;
-							datas.CopyTo(datstr);
 						}
 					}
-					sort.Clear();
-				}
-				catch (Exception e)
-				{
+					catch (Exception e)
+					{
+					}
 				}
 			}
-			((BackgroundWorker)sender).ReportProgress(0, pcnt + " индексов за " + (DateTime.Now - start).TotalSeconds.ToString("F2") + " секунд");
+			Flush(index, datas);
+
+			datas.Close();
+			index.Close();
+
+			((BackgroundWorker)sender).ReportProgress(0, pcnt.ToString("N0") + " индексов за " + (DateTime.Now - start).TotalSeconds.ToString("F2") + " секунд");
+		}
+
+		private static void Flush(MemoryStream index, MemoryStream datas)
+		{
+			if (datas.Length > 0)
+			{
+				using (var datstr = File.Create(dirout.FullName + "\\datas-" + icnt.ToString("D5")))
+				{
+					datas.Position = 0;
+					datas.CopyTo(datstr);
+				}
+			}
+			if (index.Length > 0)
+			{
+				using (var indstr = File.Create(dirout.FullName + "\\index-" + icnt.ToString("D5")))
+				{
+					index.Position = 0;
+					index.CopyTo(indstr);
+				}
+			}
 		}
 	}
 }
